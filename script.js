b647d23e3a5afbf287c55d9baed2a54ba9893ff8
Arumyan <arumyan@example.com>
2018-02-01 16:24:31 +0300

    myReduce

--- script.js
+++ script.js
@@ -36,3 +36,68 @@ function myFilter(array, callback, context){
 }
 
 console.log(myFilter(array, item => item > 4))
+
+
+
+function myMap(array, callback, context){
+
+	let func = callback;
+	let newArray = []
+
+	if(context){
+		func = callback.bind(context)
+	}
+
+	for (let i = 0; i < array.length; i++ ){
+		newArray[i] = func(array[i])
+	}
+
+	return newArray;
+}
+
+console.log(myMap(array, item => item*item))
+
+
+function mySlice(array, startIndex, endIndex){
+
+	let newArray = [];
+
+	for (let i = 0; i < array.length; i++ ){
+
+		if(!endIndex && endIndex != 0){
+			if(i >= startIndex){
+				newArray[newArray.length] = array[i];
+			}
+		}
+		else {
+			if( i >= startIndex && i < endIndex){
+				newArray[newArray.length] = array[i];
+			}
+		}
+	}
+
+	return newArray
+}
+console.log(mySlice(array, 2))
+
+function myReduce(array, callback, initialValue){
+
+	let result = array[0];
+	let index = 1
+
+	if(initialValue){
+		result = initialValue;
+		index = 0
+	}
+
+	for (let i = index; i < array.length; i++ ){
+		result = callback(result, array[i])
+	}
+
+	return result
+
+}
+
+console.log(myReduce(array, function(sum, current) {
+	return sum + current
+}, 9))
